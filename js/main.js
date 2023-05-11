b3311cf7fe152437431b1b4b292b5df243eab95e
Hao Ly <hly66@csu.fullerton.edu>
2023-05-10 17:29:14 -0700

    Done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Open / Close Mobile menu
+function mobileFunction() {
+    var x = document.querySelector(".nav-menu");
+
+    if (x.style.display === "block") 
+    {
+      x.style.display = "none";
+    } 
+    else 
+    {
+      x.style.display = "block";
+    }
+}
